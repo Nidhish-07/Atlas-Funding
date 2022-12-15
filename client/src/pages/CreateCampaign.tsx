ee8bb2ae5aa7ab52ff5cc5b9b08ebe061972a6a8
Nidhish Chauhan <rch246chauhan@gmail.com>
2022-12-15 20:25:54 +0530

    Added a campaign form

--- client/src/pages/CreateCampaign.tsx
+++ client/src/pages/CreateCampaign.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
 
-type Props = {}
+import { money } from "../assets";
+import Button from "../components/Button";
+import { checkIfImage } from "../utils";
+
+type Props = {};
 
 const CreateCampaign = (props: Props) => {
-  return (
-    <div>CreateCampaign</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default CreateCampaign
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [form, setForm] = React.useState<Object>({
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
+  });
+
+  const submitHandler = () => console.log("asd");
+  return (
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-lg sm:p-10 p-4 ">
+      {isLoading && "Loading...."}
+      <div className="flex justify-center items-center p-4 sm:min-w-[380px] bg-[#3a3a43] rounded-lg">
+        <h1 className="font-epilogue font-bold sm:text-6 text-4 leading-9 text-white">
+          Start a campaign
+        </h1>
+      </div>
+      <form
+        onSubmit={submitHandler}
+        className="w-full mt-16 flex flex-col gap-7"
+      >
+        <div className="flex flex-wrap gap-10"></div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateCampaign;
